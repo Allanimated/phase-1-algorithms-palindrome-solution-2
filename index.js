d163ab354b5fc4d9feef25ba23eb1e82413ef891
Allan <allcreativewrites@gmail.com>
2023-10-04 14:06:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  // iterate from the beginning of the string to the middle of the string
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    // compare the letter we're iterating over to the corresponding letter at the end of the string
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      // if the letters don't match, return false
+      return false;
+    }
+  }
 
+  // if we reach the middle, and all the letters match, return true
+  return true;
+}
 /* 
-  Add your pseudocode here
+  // iterate from the beginning of the string to the middle of the string
+  // compare the letter we're iterating over to the corresponding letter at the end of the string
+  // if the letters don't match, return false
+  // if we reach the middle, and all the letters match, return true
 */
 
 /*
-  Add written explanation of your solution here
+  this function compares letters from the left side of the word with those on the right side of the word
+  it iterates each letter to the middle of the word comparing each one of them with the corresponding index 
+  from the right. It is has an efficient perfomance as it optimizes space and time
 */
 
 // You can run `node index.js` to view these console logs
